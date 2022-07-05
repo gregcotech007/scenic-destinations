e95f29ac86276c2186e8ada9ae91e7e8c782ffea
gregcotech007 <commins.greg@outlook.com>
2022-07-05 13:22:31 +0000

    update user_form typo on views.py

--- users/views.py
+++ users/views.py
@@ -24,7 +24,7 @@ def profile(request):
         profile_form = ProfileUpdateForm(request.POST,
                                    request.FILES,
                                    instance=request.user.profile)
-        if user_form.is_valid() and p_form.is_valid():
+        if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
             messages.success(request, f'Your account has been updated!')
@@ -35,8 +35,8 @@ def profile(request):
         profile_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
-        'user_form': u_form,
-        'profile_form': p_form
+        'user_form': user_form,
+        'profile_form': profile_form
     }
 
     return render(request, 'users/profile.html', context)
