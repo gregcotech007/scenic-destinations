96b1f88a40d56384b771fee67f241a67e97e20e1
gregcotech007 <commins.greg@outlook.com>
2022-07-16 10:35:58 +0000

    Removed slug and comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,25 +1,25 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post # Removed Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'status', 'created_on') # Removed 'slug
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
+    # prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
    
-    list_display = ('name', 'body', 'post', 'created_on', 'approved')
-    list_filter = ('approved', 'created_on')
-    search_fields = ('name', 'email', 'body')
-    actions = ['approve_comments']
+#     list_display = ('name', 'body', 'post', 'created_on', 'approved')
+#     list_filter = ('approved', 'created_on')
+#     search_fields = ('name', 'email', 'body')
+#     actions = ['approve_comments']
 
-    def approve_comments(self, request, queryset):
-        queryset.update(approved=True)
+#     def approve_comments(self, request, queryset):
+#         queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,6 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -29,17 +28,17 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
-class Comment(models.Model):
+# class Comment(models.Model):
 
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
-    created_on = models.DateTimeField(default=timezone.now)
-    approved = models.BooleanField(default=False)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+#     name = models.CharField(max_length=80)
+#     email = models.EmailField()
+#     body = models.TextField()
+#     created_on = models.DateTimeField(default=timezone.now)
+#     approved = models.BooleanField(default=False)
 
-    class Meta:
-        ordering = ['created_on']
+#     class Meta:
+#         ordering = ['created_on']
 
-        def __str__(self):
-            return f"Comment {self.body} by {self.name}"
+#         def __str__(self):
+#             return f"Comment {self.body} by {self.name}"
