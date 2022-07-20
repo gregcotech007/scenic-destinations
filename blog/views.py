32ebc643f5f279e5781d7a6e0a1ba7ec8d84230e
gregcotech007 <commins.greg@outlook.com>
2022-07-20 22:42:09 +0000

    Add fix to likes

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
