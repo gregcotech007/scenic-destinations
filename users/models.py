bad7e408b8d2b7eecf1d409617014771795e45ab
gregcotech007 <commins.greg@outlook.com>
2023-02-27 22:44:14 +0000

    Add css styling to profile

--- users/models.py
+++ users/models.py
@@ -5,17 +5,7 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profile_default_image.jpg', upload_to='profile_images')
+    image = models.ImageField(default='no_image.jpeg', upload_to='profile_images')
 
     def __str__(self):
         return f'{self.user.username} Profile'
-
-    # def save(self):
-    #     super().save()
-
-    #     img = Image.open(self.image.path)
-
-    #     if img.height > 300 or img.width > 300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image.path)
