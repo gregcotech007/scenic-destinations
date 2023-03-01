8619f7366891a00a683b153819ab409ddc469eaa
gregcotech007 <commins.greg@outlook.com>
2023-03-01 21:05:21 +0000

    Update form styling

--- users/views.py
+++ users/views.py
@@ -3,6 +3,23 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.dispatch import receiver
+
+
+@receiver(user_logged_in)
+def on_user_logged_in(sender, request, user, **kwargs):
+    if user:
+        msg = 'You have securely logged in with {email}. Thank you for visiting.'.format(email=user.email)
+    messages.add_message(request, messages.INFO, msg)
+
+
+@receiver(user_logged_out)
+def on_user_logged_out(sender, request, user, **kwargs):
+    if user:
+        msg = 'You have securely logged out {email}. Thank you for visiting.'.format(email=user.email)
+        messages.add_message(request, messages.INFO, msg)
+
 
 def register(request):
     if request.method == 'POST':
